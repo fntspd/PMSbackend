fdc05f0b77cf35d475832b78cf1bff17b9e51e4f
Sumit Saha <sumitsaha395@gmail.com>
2024-02-19 01:58:04 +0530

    1st

---
+++ src/main/java/com/example/pmsbackend/PmsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.pmsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PmsBackendApplication.class, args);
+	}
+
+}
