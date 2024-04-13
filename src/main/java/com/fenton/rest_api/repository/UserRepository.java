a9bc9dcc753f7c58f8eead3a9d3d9f8d1be2765e
Sarthak <sarthakgarai05@gmail.com>
2024-04-14 02:04:36 +0530

    Added Role for role based authorization

---
+++ src/main/java/com/fenton/rest_api/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package com.fenton.rest_api.repository;
+
+import com.fenton.rest_api.Model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+    Optional<User> findByUsername(String username);
+
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
+}
