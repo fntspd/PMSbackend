91a868068706a26ea7d7cc3000d4ca61385f59dd
Sarthak <sarthakgarai05@gmail.com>
2024-04-13 20:37:36 +0530

    Added Role for role based authorization

--- src/main/java/com/fenton/rest_api/Model/User.java
+++ src/main/java/com/fenton/rest_api/Model/User.java
@@ -1,4 +1,41 @@
 package com.fenton.rest_api.Model;
 
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
+@Table(	name = "users",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = "username"),
+                @UniqueConstraint(columnNames = "email")})
 public class User {
+    @jakarta.persistence.Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long id;
+    @Column(nullable = false,length = 20)
+    private String username;
+    @Column(nullable = false,length = 50)
+    private String email;
+    @Column(nullable = false,length = 50)
+    private  String password;
+
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "user_roles"
+            ,joinColumns = @JoinColumn(name = "usr_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> role = new HashSet<>();
+
+
 }
+
+
